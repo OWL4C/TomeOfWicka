8feca5d333c24b650ba6c2ab70a71adcda43754d
OWL4C <124401812+OWL4C@users.noreply.github.com>
2025-02-11 11:05:27 -0500

    Merge

--- TomeOfWicka/ModEntry.cs
+++ TomeOfWicka/ModEntry.cs
@@ -4,12 +4,23 @@ using StardewModdingAPI;
 using StardewModdingAPI.Events;
 using StardewModdingAPI.Utilities;
 using StardewValley;
+// Using StardewUI for Ingame UI
+using StardewUI.Framework;
+
 
 namespace TomeOfWicka
 {
     /// <summary>The mod entry point.</summary>
     internal sealed class ModEntry : Mod
     {
+
+        // Initialized in GameLaunched
+        private string viewAssetPrefix = null!;
+        private IViewEngine viewEngine = null!;
+        
+
+
+
         /*********
         ** Public methods
         *********/
@@ -18,8 +29,21 @@ namespace TomeOfWicka
         public override void Entry(IModHelper helper)
         {
             helper.Events.Input.ButtonPressed += this.OnButtonPressed;
+            helper.Events.GameLoop.GameLaunched += this.GameLaunched;
+            viewAssetPrefix = $"Mods/{ModManifest.UniqueID}/Views";
+
+
         }
 
+        // Setup ViewEngine on Launch
+        private void GameLaunched(object? _sender, GameLaunchedEventArgs _e){
+            viewEngine = Helper.ModRegistry.GetApi<IViewEngine>("focustense.StardewUI")!;
+            viewEngine.RegisterSprites($"Mods/{ModManifest.UniqueID}/Sprites", "assets/sprites");
+            viewEngine.RegisterViews(viewAssetPrefix, "assets/views");
+            viewEngine.EnableHotReloadingWithSourceSync();
+            viewEngine.PreloadAssets();
+            this.Monitor.Log($"{viewAssetPrefix}");
+        }
 
         /*********
         ** Private methods
@@ -32,9 +56,14 @@ namespace TomeOfWicka
             // ignore if player hasn't loaded a save yet
             if (!Context.IsWorldReady)
                 return;
+            if (e.Button == SButton.L)
+               ShowConfig();
 
             // print button presses to the console window
             this.Monitor.Log($"{Game1.player.Name} pressed {e.Button}.", LogLevel.Debug);
         }
+
+
+        
     }
 }
